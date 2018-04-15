d57ca1448658a1d322a2fae1dbd6fafd4e54f2dd
gitWxk <wxk@email.com>
2018-04-15 21:57:05 +0800

    gx
    Gx

---
+++ XKPerson6/PersonGX.m
@@ -0,0 +1,13 @@
+//
+//  PersonGX.m
+//  xkpp6
+//
+//  Created by 王晓康 on 2018/4/15.
+//  Copyright © 2018年 wangxiaokang. All rights reserved.
+//
+
+#import "PersonGX.h"
+
+@implementation PersonGX
+
+@end
